7f1846d9f26ebafcd5705a918642968e8d0d78ec
BaxterGovan <31545640+BaxterGovan@users.noreply.github.com>
2022-01-27 15:16:42 -0500

    Update Unsub.js

--- Script/Unsub.js
+++ Script/Unsub.js
@@ -1,2 +1,44 @@
 import COMMAND_STRING from "./secrets.js";
 
+function load(email) {
+ $.ajax({
+    url: "https://app.bluefolder.com/api/2.0/customers/list.aspx",
+   headers: {
+        'Authorization':'Basic '+COMMAND_STRING,
+        'Content-Type':'text/xml'
+    },
+    type: "GET",
+    data: "<request>" +
+              "<customerList>" +
+                "<listType>basic</listType>" +
+              "</customerList>" +
+          "</request>",
+    success: function (result) {
+      console.log(result); 
+    },
+    error: function (error) {
+      console.log(result);
+    }  
+ })
+  
+}
+
+
+
+
+/*
+$.ajax({
+    url: 'YourRestEndPoint',
+    headers: {
+        'Authorization':'Basic xxxxxxxxxxxxx',
+        'X-CSRF-TOKEN':'xxxxxxxxxxxxxxxxxxxx',
+        'Content-Type':'application/json'
+    },
+    method: 'POST',
+    dataType: 'json',
+    data: YourData,
+    success: function(data){
+      console.log('succes: '+data);
+    }
+  });
+  */
