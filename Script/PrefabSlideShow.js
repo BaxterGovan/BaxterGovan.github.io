998cbe9a61a5a41251a9eacee6c9b28aa8a1bf9d
BaxterGovan <31545640+BaxterGovan@users.noreply.github.com>
2022-01-31 09:08:34 -0500

    Update PrefabSlideShow.js

--- Script/PrefabSlideShow.js
+++ Script/PrefabSlideShow.js
@@ -31,7 +31,67 @@
 					setTimeout(function() {addOpacity1(photo)},1000);
 				}
 				
-				track1=0;
+				track1++;
+				
+			}
+			else if( track1 == 2) {
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {minusOpacity1(photo)},0);
+				}
+				setTimeout(function() {theSwitch3(photo)},1000);
+
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {addOpacity1(photo)},1000);
+				}
+				
+				track1++;
+				
+			}
+			else if( track1 == 3) {
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {minusOpacity1(photo)},0);
+				}
+				setTimeout(function() {theSwitch4(photo)},1000);
+
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {addOpacity1(photo)},1000);
+				}
+				
+				track1++;
+				
+			}
+			else if( track1 == 4) {
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {minusOpacity1(photo)},0);
+				}
+				setTimeout(function() {theSwitch5(photo)},1000);
+
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {addOpacity1(photo)},1000);
+				}
+				
+				track1++;
+				
+			}
+			else if( track1 == 5) {
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {minusOpacity1(photo)},0);
+				}
+				setTimeout(function() {theSwitch6(photo)},1000);
+
+				for(let i=0;i<1;i++)
+				{
+					setTimeout(function() {addOpacity1(photo)},1000);
+				}
+				
+				track1 = 0;
 				
 			}
 			
@@ -51,5 +111,17 @@
  }
  function theSwitch2(event1) {
 	event1.src = "/Photos/Prefab/PrefabSpool.png";
+ }
+ function theSwitch3(event1) {
+	event1.src = "/Photos/Prefab/Prefab-sm1.jpg";
+ }
+ function theSwitch4(event1) {
+	event1.src = "/Photos/Prefab/Prefab-sm2.jpgg";
+ }
+ function theSwitch5(event1) {
+	event1.src = "/Photos/Prefab/Prefab-sm3.jpg";
+ }
+ function theSwitch6(event1) {
+	event1.src = "/Photos/Prefab/Prefab-sm4.jpg";
  }
 
