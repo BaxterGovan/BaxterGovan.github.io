af8239ed69a6a8d33b39e0b00fb1e287652628a5
BaxterGovan <31545640+BaxterGovan@users.noreply.github.com>
2022-01-31 12:16:29 -0500

    Update award_ss_buttons2.js

--- Script/award_ss_buttons2.js
+++ Script/award_ss_buttons2.js
@@ -5,6 +5,14 @@ var pht6 = document.getElementById("pht6");
 var phttxt4 = document.getElementById("phttxt4");
 var phttxt5 = document.getElementById("phttxt5");
 var phttxt6 = document.getElementById("phttxt6");
+
+
+pht4.style.maxHeight = "436px";
+pht5.style.maxHeight = "436px";
+pht6.style.maxHeight = "436px";
+phttxt4.style.marginTop = "-20px";
+phttxt5.style.marginTop = "-20px";
+phttxt6.style.marginTop = "-20px";
 function awardsSSPrev2() {
 
 	if( count2 == 0)
@@ -15,7 +23,9 @@ function awardsSSPrev2() {
 		pht5.src = "/Photos/Awards/Glass/Award2020OSPA.png";
 		pht6.src = "/Photos/Awards/Glass/Award2021NECA.png";
 		phttxt4.innerText = "2020 NECA Project Excellence Award";
+		phttxt4.style.marginTop = "-20px";
 		phttxt5.innerText = "2020 Turner Outstanding Subcontractor Performance Award";
+		phttxt5.style.fontSize = "18px";
 		phttxt6.innerText = "2021 NECA Project Excellence Award";
 	}
 	else if( count2 == 1)
@@ -26,7 +36,9 @@ function awardsSSPrev2() {
 		pht5.src = "/Photos/Awards/Glass/Award2004BGSC.png";
 		pht6.src = "/Photos/Awards/Glass/Award2006BGSC.png";
 		phttxt4.innerText = "2001 Build Georgia Specialty Contractor Award";
+		phttxt4.style.marginTop = "-50px";
 		phttxt5.innerText = "2004 Build Georgia Specialty Contractor Award";
+		phttxt5.style.fontSize = "20px";
 		phttxt6.innerText = "2006 Build Georgia Specialty Contractor Award";
 	}
 	else if( count2 == 2)
@@ -37,6 +49,7 @@ function awardsSSPrev2() {
 		pht5.src = "/Photos/Awards/Glass/Award2008BGSC.png";
 		pht6.src = "/Photos/Awards/Glass/Award2009BGSC.png";
 		phttxt4.innerText = "2007 Build Georgia Specialty Contractor Award";
+		phttxt4.style.marginTop = "-20px";
 		phttxt5.innerText = "2008 Build Georgia Specialty Contractor Award";
 		phttxt6.innerText = "2009 Build Georgia Specialty Contractor Award First Place";
 	}
@@ -49,6 +62,7 @@ function awardsSSPrev2() {
 		phttxt4.innerText = "2010 Build Georgia Specialty Contractor Award First Place";
 		phttxt5.innerText = "2012 AGC Build Georgia Specialty Contractor Award First Place";
 		phttxt6.innerText = "2013 Turner Outstanding Subcontractor Performance Award";
+		phttxt6.style.fontSize = "18px";
 	}
 	else if( count2 == 4)
 	{
